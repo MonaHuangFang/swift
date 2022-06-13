54642f7e0aabe58e98746010fb6f8cd005bb1bde
MonaHuangFang <1014759092@qq.com>
2022-06-13 18:47:29 +0800

    泛型编辑

--- playground/Generic.playground/Contents.swift
+++ playground/Generic.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+    let temp = a
+    a = b
+    b = temp
+}
+
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element){
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+print(stackOfStrings.topItem!)
+
